40cd0a6a6bd36e2958505ef50f9c86c8115f81ae
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-04 22:54:26 +0200

    parser should return PlayerStatistic object instead of BeautifulSoup

--- player_statistics/parsers/player_statistics_html_parser.py
+++ player_statistics/parsers/player_statistics_html_parser.py
@@ -1,8 +1,29 @@
-from player_statistics.parsers.base_parser import BaseParser
 from bs4 import BeautifulSoup
+
+from player_statistics.parsers.base_parser import BaseParser
+from player_statistics.models import Player, PlayerStatistics
 
 
 class PlayerStatisticsHtmlParser(BaseParser):
     def parse(self, page):
         soup = BeautifulSoup(page, "html.parser")
-        return soup
+        player_stat = self.parse_player_statistics(soup)
+        return [player_stat]
+
+    @staticmethod
+    def parse_player_statistics(soup: BeautifulSoup) -> PlayerStatistics:
+        """
+        :param soup: BeautifulSoup of player statistics page
+        :return: parsed player statistics
+        :rtype: PlayerStatistics
+        """
+        parsed_player_stat = PlayerStatistics()
+        parsed_player_stat.player = Player()
+        players_stat_table = soup.find(id="playersStatisticsTable").tbody
+
+        # 1 row per player
+        player_stat = players_stat_table.find_all('tr')
+        player_stat_values = player_stat[0].find_all('td')
+        parsed_player_stat.player.position = player_stat_values[1].string
+
+        return parsed_player_stat
--- player_statistics/tests/unit/test_statistics_html_parser.py
+++ player_statistics/tests/unit/test_statistics_html_parser.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
 import os
 
+from django.test import TestCase
+
+from player_statistics.models import Player, PlayerStatistics
 from player_statistics.parsers.player_statistics_html_parser import PlayerStatisticsHtmlParser
 
 TESTDATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'assets')
@@ -8,9 +10,18 @@ TESTDATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pard
 
 class StatisticsHtmlParserTest(TestCase):
     def setUp(self):
-        self.testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'))
-
-    def test_parser_can_parse_player_statistics(self):
+        testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'))
         parser = PlayerStatisticsHtmlParser()
-        soup = parser.parse(self.testdata)
-        self.assertEqual('OFM Spielerstatistik', soup.title.string)
+        self.player_stat_list = parser.parse(testdata)
+
+    def test_parser_returns_list_of_player_statistics(self):
+        self.assertEquals(type(self.player_stat_list), list)
+        self.assertEquals(type(self.player_stat_list[0]), PlayerStatistics)
+
+    def test_parsed_player_stat_has_player(self):
+        first_player = self.player_stat_list[0].player
+        self.assertEquals(type(first_player), Player)
+
+    def test_parsed_player_stat_contains_correct_player_pos(self):
+        first_player = self.player_stat_list[0].player
+        self.assertEquals('TW', first_player.position)
