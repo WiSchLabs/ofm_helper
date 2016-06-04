9d3098ffd530d99fa20de503081a9e55916e5d44
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-04 18:24:59 +0200

    added test for parsing example player statistics html file

--- player_statistics/tests/unit/test_statistics_html_parser.py
+++ player_statistics/tests/unit/test_statistics_html_parser.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
+import os
 
 from player_statistics.parsers.player_statistics_html_parser import PlayerStatisticsHtmlParser
+
+TESTDATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'assets')
 
 
 class StatisticsHtmlParserTest(TestCase):
     def setUp(self):
-        pass
+        self.testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'))
 
-    def test_chromium_html_parser(self):
-        pass
-        #parser = PlayerStatisticsHtmlParser()
-        #soup = parser.parse("")
+    def test_parser_can_parse_player_statistics(self):
+        parser = PlayerStatisticsHtmlParser()
+        soup = parser.parse(self.testdata)
+        self.assertEqual('OFM Spielerstatistik', soup.title.string)
