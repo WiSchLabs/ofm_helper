fe98fb1d9f075ec0013f5c7672d4136b1298cdca
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-05 09:36:33 +0200

    enhanced player stat parser (with TDD) added second test document (currrently not used)

--- player_statistics/parsers/player_statistics_html_parser.py
+++ player_statistics/parsers/player_statistics_html_parser.py
@@ -7,23 +7,40 @@ from player_statistics.models import Player, PlayerStatistics
 class PlayerStatisticsHtmlParser(BaseParser):
     def parse(self, page):
         soup = BeautifulSoup(page, "html.parser")
-        player_stat = self.parse_player_statistics(soup)
-        return [player_stat]
+        return self.parse_player_statistics_html(soup)
 
-    @staticmethod
-    def parse_player_statistics(soup: BeautifulSoup) -> PlayerStatistics:
+    def parse_player_statistics_html(self, soup: BeautifulSoup) -> PlayerStatistics:
         """
         :param soup: BeautifulSoup of player statistics page
         :return: parsed player statistics
         :rtype: PlayerStatistics
         """
-        parsed_player_stat = PlayerStatistics()
-        parsed_player_stat.player = Player()
+        parsed_player_stat_list = []
         players_stat_table = soup.find(id="playersStatisticsTable").tbody
 
         # 1 row per player
-        player_stat = players_stat_table.find_all('tr')
-        player_stat_values = player_stat[0].find_all('td')
-        parsed_player_stat.player.position = player_stat_values[1].string
+        player_list = players_stat_table.find_all('tr')
+        for player_row in player_list:
+            parsed_player_stat = self.parse_player_stat_row(player_row)
+            parsed_player_stat_list.append(parsed_player_stat)
+
+        return parsed_player_stat_list
+
+    def parse_player_stat_row(self, player_row):
+        parsed_player_stat = PlayerStatistics()
+        player_stat_values = player_row.find_all('td')
+
+        parsed_player_stat.strength = player_stat_values[5].get_text().strip(' ')
+        parsed_player_stat.freshness = player_stat_values[6].get_text()
+        parsed_player_stat.games_in_season = player_stat_values[7].get_text()
+        parsed_player_stat.goals_in_season = player_stat_values[8].get_text()
+
+        parsed_player_stat.player = self.parse_player(player_stat_values)
 
         return parsed_player_stat
+
+    def parse_player(self, player_stat_values):
+        parsed_player = Player()
+        parsed_player.position = player_stat_values[1].get_text()
+        parsed_player.name = player_stat_values[3].a.get_text().strip('\n').strip('\t').strip(' ')
+        return parsed_player
--- player_statistics/tests/unit/test_statistics_html_parser.py
+++ player_statistics/tests/unit/test_statistics_html_parser.py
@@ -10,18 +10,37 @@ TESTDATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pard
 
 class StatisticsHtmlParserTest(TestCase):
     def setUp(self):
-        testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'))
+        testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'), encoding='utf8')
         parser = PlayerStatisticsHtmlParser()
         self.player_stat_list = parser.parse(testdata)
+        self.first_player_stat = self.player_stat_list[0]
+        self.first_player = self.first_player_stat.player
 
     def test_parser_returns_list_of_player_statistics(self):
         self.assertEquals(type(self.player_stat_list), list)
         self.assertEquals(type(self.player_stat_list[0]), PlayerStatistics)
 
-    def test_parsed_player_stat_has_player(self):
-        first_player = self.player_stat_list[0].player
-        self.assertEquals(type(first_player), Player)
+    def test_parsed_player_stat_contains_all_entries(self):
+        self.assertEquals(12, len(self.player_stat_list))
+        for player_stat in self.player_stat_list:
+            self.assertEquals(type(player_stat), PlayerStatistics)
+            self.assertEquals(type(player_stat.player), Player)
 
     def test_parsed_player_stat_contains_correct_player_pos(self):
-        first_player = self.player_stat_list[0].player
-        self.assertEquals('TW', first_player.position)
+        self.assertEquals('TW', self.first_player.position)
+
+    def test_parsed_player_stat_contains_correct_player_name(self):
+        self.assertEquals('Chrístos Tsigas', self.first_player.name)
+
+    def test_parsed_player_stat_contains_correct_player_strength(self):
+        self.assertEquals('15', self.first_player_stat.strength)
+
+    def test_parsed_player_stat_contains_correct_player_freshness(self):
+        self.assertEquals('47', self.first_player_stat.freshness)
+
+    def test_parsed_player_stat_contains_correct_played_games(self):
+        self.assertEquals('29', self.first_player_stat.games_in_season)
+
+    def test_parsed_player_stat_contains_correct_scored_goals(self):
+        player_stat = self.player_stat_list[3]
+        self.assertEquals('4', player_stat.goals_in_season)
