1409f22a6682b23f4b9e2241bfeca0f12a507ace
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-05 11:14:42 +0200

    refactoring

--- player_statistics/parsers/player_statistics_html_parser.py
+++ player_statistics/parsers/player_statistics_html_parser.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 
-from player_statistics.parsers.base_parser import BaseParser
 from player_statistics.models import Player, PlayerStatistics
+from player_statistics.parsers.base_parser import BaseParser
+
+MULTIVALUE_SEPARATOR = ' / '
 
 
 class PlayerStatisticsHtmlParser(BaseParser):
@@ -34,16 +36,22 @@ class PlayerStatisticsHtmlParser(BaseParser):
         parsed_player_stat.freshness = player_stat_values[6].get_text()
         parsed_player_stat.games_in_season = player_stat_values[7].get_text()
         parsed_player_stat.goals_in_season = player_stat_values[8].get_text()
-        parsed_player_stat.won_tacklings_in_season = player_stat_values[9].get_text().split(' / ')[0]
-        parsed_player_stat.lost_tacklings_in_season = player_stat_values[9].get_text().split(' / ')[1]
-        parsed_player_stat.won_friendly_tacklings_in_season = player_stat_values[10].get_text().split(' / ')[0]
-        parsed_player_stat.lost_friendly_tacklings_in_season = player_stat_values[10].get_text().split(' / ')[1]
-        parsed_player_stat.yellow_cards_in_season = player_stat_values[13].get_text().split(' / ')[0]
-        parsed_player_stat.red_cards_in_season = player_stat_values[13].get_text().split(' / ')[1]
-        parsed_player_stat.ep = player_stat_values[14].get_text().strip('\n').split('\n')[0].strip('\n').strip('\t').replace('.', '')
-        parsed_player_stat.tp = player_stat_values[16].get_text().strip('\n').split('\n')[0].strip('\n').strip('\t').replace('.', '')
+        parsed_player_stat.won_tacklings_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[9], 0)
+        parsed_player_stat.lost_tacklings_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[9], 1)
+        parsed_player_stat.won_friendly_tacklings_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[10], 0)
+        parsed_player_stat.lost_friendly_tacklings_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[10], 1)
+        parsed_player_stat.yellow_cards_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[13], 0)
+        parsed_player_stat.red_cards_in_season = \
+            self.get_value_from_multivalue_table_cell(player_stat_values[13], 1)
+        parsed_player_stat.ep = self.get_ep_tp_value_from_table_cell(player_stat_values[14])
+        parsed_player_stat.tp = self.get_ep_tp_value_from_table_cell(player_stat_values[16])
         parsed_player_stat.awp = player_stat_values[18].span.get_text().replace('.', '')
-        parsed_player_stat.equity = player_stat_values[21].get_text().strip('\n').strip('\t').replace('.', '').replace('€', '').strip(' ')
+        parsed_player_stat.equity = self.get_equity_value_from_table_cell(player_stat_values[21])
 
         parsed_player_stat.player = self.parse_player(player_stat_values)
 
@@ -54,3 +62,12 @@ class PlayerStatisticsHtmlParser(BaseParser):
         parsed_player.position = player_stat_values[1].get_text()
         parsed_player.name = player_stat_values[3].a.get_text().strip('\n').strip('\t').strip(' ')
         return parsed_player
+
+    def get_value_from_multivalue_table_cell(self, field, index):
+        return field.get_text().split(MULTIVALUE_SEPARATOR)[index]
+
+    def get_ep_tp_value_from_table_cell(self, field):
+        return field.get_text().strip('\n').split('\n')[0].strip('\n').strip('\t').replace('.', '')
+
+    def get_equity_value_from_table_cell(self, field):
+        return field.get_text().strip('\n').strip('\t').replace('.', '').replace('€', '').strip(' ')
