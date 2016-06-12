9b5f02406756c0d2420ec3e636e366f09689cc33
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-12 12:13:36 +0200

    first test implementation of login with selenium

---
+++ core/tests/unit/test_login.py
@@ -0,0 +1,40 @@
+import configparser
+import os
+import time
+import unittest
+
+from selenium import webdriver
+
+TESTDATA_PATH = 'core/tests/assets'
+
+
+class CreateCoreModelsTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get("http://v7.www.onlinefussballmanager.de/")
+        config = configparser.ConfigParser()
+        config.read(os.path.join(TESTDATA_PATH, 'config.txt'))
+        self.login_user = config.get("configuration", "login")
+        self.login_password = config.get("configuration", "password")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login(self):
+        if self.login_user == "XXX":
+            return
+        self.assertNotIn("OFM", self.browser.title)
+        self.login()
+        self.assertIn("OFM", self.browser.title)
+
+    def login(self):
+        link_login_account = self.browser.find_element_by_xpath("//div[@id='register_div']//a")
+        link_login_account.click()
+        time.sleep(1)
+        login_field_user = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='login']")
+        login_field_user.send_keys(self.login_user)
+        login_field_password = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='password']")
+        login_field_password.send_keys(self.login_password)
+        login_button = self.browser.find_element_by_xpath("//form[@id='login_form']//button[@id='logingrafikbutton']")
+        login_button.click()
+        time.sleep(1)
