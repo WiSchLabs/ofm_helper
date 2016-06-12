66d9eda5e1b3ee54b3e6e721b7f8d5e4036b7724
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-12 15:31:53 +0200

    changed tests

--- core/configuration_provider.py
+++ core/configuration_provider.py
@@ -9,7 +9,7 @@ class ConfigurationProvider:
         self.config = configparser.ConfigParser()
         self.config.read(settings.CONFIGURATION_FILE)
 
-    def get(self, config_group, config_param):
-        if os.environ.get(config_param):
+    def get(self, config_group, config_param, use_env_vars=True):
+        if use_env_vars and os.environ.get(config_param):
             return os.environ[config_param]
         return self.config.get(config_group, config_param)
--- core/tests/unit/test_configuration_provider.py
+++ core/tests/unit/test_configuration_provider.py
@@ -6,14 +6,15 @@ from core.configuration_provider import ConfigurationProvider
 
 class ConfigurationProviderTest(unittest.TestCase):
     def setUp(self):
+
         self.cp = ConfigurationProvider()
 
     def test_get_login_username(self):
-        username = self.cp.get('credentials', 'OFM_USERNAME')
+        username = self.cp.get('credentials', 'OFM_USERNAME', use_env_vars=False)
         self.assertEqual(username, 'XXX')
 
     def test_get_login_password(self):
-        password = self.cp.get('credentials', 'OFM_PASSWORD')
+        password = self.cp.get('credentials', 'OFM_PASSWORD', use_env_vars=False)
         self.assertEqual(password, '1234')
 
     def test_override_env_variable_and_get_correct_result(self):
