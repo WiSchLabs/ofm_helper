9c1218aa2f42441ea568b1bf0376111cb9a11897
Michael Wittig <witmic1@gmail.com>
2016-06-14 13:05:33 +0200

    basic homepage with bootstrap

--- ofm_helper/urls.py
+++ ofm_helper/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('core.urls'), name='core'),
 ]
 
 if settings.DEBUG is False:  # if DEBUG is True it will be served automatically
