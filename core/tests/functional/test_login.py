e3b5297d57de45f5f7f57d487769cfa4e0b4238f
Michael Wittig <witmic1@gmail.com>
2016-06-14 17:34:30 +0200

    break login functionality out of test class

--- core/tests/functional/test_login.py
+++ core/tests/functional/test_login.py
@@ -1,47 +1,24 @@
-import time
 import unittest
 
+from core.web import ofm_page_constants
+from core.web.site_manager import SiteManager
 from django.conf import settings
-from selenium import webdriver
-
-from core.configuration_provider import ConfigurationProvider
-
-TESTDATA_PATH = 'core/config'
 
 
-class CreateCoreModelsTest(unittest.TestCase):
+class LoginTest(unittest.TestCase):
     def setUp(self):
-        cfg = ConfigurationProvider()
-        if settings.USE_DISPLAY_FOR_AWS:
-            from pyvirtualdisplay import Display
-            self.display = Display(visible=0, size=(1024, 768))
-            self.display.start()
-        if cfg.get("phantomjs", "PHANTOMJS_PATH"):
-            self.browser = webdriver.PhantomJS(executable_path=r'' + cfg.get("phantomjs", "PHANTOMJS_PATH"))
-        else:
-            self.browser = webdriver.PhantomJS()
-        self.browser.get("http://v7.www.onlinefussballmanager.de/")
-        self.login_user = cfg.get("credentials", "OFM_USERNAME")
-        self.login_password = cfg.get("credentials", "OFM_PASSWORD")
+        self.site_manager = SiteManager()
+        self.site_manager.login()
 
     def tearDown(self):
-        self.browser.quit()
+        self.site_manager.browser.quit()
         if settings.USE_DISPLAY_FOR_AWS:
-            self.display.stop()
+            self.site_manager.display.stop()
 
     def test_login(self):
-        self.assertNotIn("OFM", self.browser.title)
-        self.login()
-        self.assertIn("OFM", self.browser.title)
-
-    def login(self):
-        link_login_account = self.browser.find_element_by_xpath("//div[@id='register_div']//a")
-        link_login_account.click()
-        time.sleep(1)
-        login_field_user = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='login']")
-        login_field_user.send_keys(self.login_user)
-        login_field_password = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='password']")
-        login_field_password.send_keys(self.login_password)
-        login_button = self.browser.find_element_by_xpath("//form[@id='login_form']//button[@id='logingrafikbutton']")
-        login_button.click()
-        time.sleep(1)
+        self.assertIn("OFM", self.site_manager.browser.title)
+        self.site_manager.browser.get(ofm_page_constants.HEAD)
+        self.assertIn("Spieltag", self.site_manager.browser.page_source)
+        self.assertIn("Saison", self.site_manager.browser.page_source)
+        self.assertIn("Team", self.site_manager.browser.page_source)
+        self.assertIn("Liga", self.site_manager.browser.page_source)
---
+++ core/web/site_manager.py
@@ -0,0 +1,52 @@
+import time
+
+from core.configuration_provider import ConfigurationProvider
+from core.web import ofm_page_constants
+from django.conf import settings
+from selenium import webdriver
+
+
+class SiteManager:
+    def __init__(self):
+        cfg = ConfigurationProvider()
+
+        self.browser = webdriver.PhantomJS()
+
+        self._handle_aws_display_bug()
+        self._handle_phantomjs_bug(cfg)
+
+        self._login_user = cfg.get("credentials", "OFM_USERNAME")
+        self._login_password = cfg.get("credentials", "OFM_PASSWORD")
+
+    def login(self):
+        self.browser.get(ofm_page_constants.LOGIN)
+        self._switch_to_login_div()
+
+        self._insert_login_credentials()
+        self._click_login_button()
+
+    def _handle_phantomjs_bug(self, cfg):
+        if cfg.get("phantomjs", "PHANTOMJS_PATH"):
+            self.browser = webdriver.PhantomJS(executable_path=r'' + cfg.get("phantomjs", "PHANTOMJS_PATH"))
+
+    def _handle_aws_display_bug(self):
+        if settings.USE_DISPLAY_FOR_AWS:
+            from pyvirtualdisplay import Display
+            self.display = Display(visible=0, size=(1024, 768))
+            self.display.start()
+
+    def _switch_to_login_div(self):
+        link_login_account = self.browser.find_element_by_xpath("//div[@id='register_div']//a")
+        link_login_account.click()
+        time.sleep(1)
+
+    def _insert_login_credentials(self):
+        login_field_user = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='login']")
+        login_field_user.send_keys(self._login_user)
+        login_field_password = self.browser.find_element_by_xpath("//form[@id='login_form']//input[@id='password']")
+        login_field_password.send_keys(self._login_password)
+
+    def _click_login_button(self):
+        login_button = self.browser.find_element_by_xpath("//form[@id='login_form']//button[@id='logingrafikbutton']")
+        login_button.click()
+        time.sleep(1)
