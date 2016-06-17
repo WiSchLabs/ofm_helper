36fdbba84cf384ae3ec3011b5526e2cd70791387
Michael Wittig <witmic1@gmail.com>
2016-06-17 15:49:02 +0200

    re-fail test

--- player_statistics/tests/functional/test_statistics_html_parser.py
+++ player_statistics/tests/functional/test_statistics_html_parser.py
@@ -10,12 +10,12 @@ from player_statistics.parsers.player_statistics_html_parser import PlayerStatis
 class StatisticsHtmlParserTest(TestCase):
     def setUp(self):
         self.site_manager = SiteManager()
-    #    self.site_manager.login()
-    #    self.site_manager.browser.get(Constants.TEAM.PLAYER_STATISTICS)
-    #    parser = PlayerStatisticsHtmlParser()
-    #    self.player_stat_list = parser.parse(self.site_manager.browser.page_source)
-    #    self.first_player_stat = self.player_stat_list[0]
+        self.site_manager.login()
+        self.site_manager.browser.get(Constants.TEAM.PLAYER_STATISTICS)
+        parser = PlayerStatisticsHtmlParser()
+        self.player_stat_list = parser.parse(self.site_manager.browser.page_source)
+        self.first_player_stat = self.player_stat_list[0]
 
-    #def test_parsed_player_stat_contains_all_foreign_keys(self):
-    #    self.assertEquals(type(self.first_player_stat), PlayerStatistics)
-    #    self.assertEquals(type(self.first_player_stat.player), Player)
+    def test_parsed_player_stat_contains_all_foreign_keys(self):
+        self.assertEquals(type(self.first_player_stat), PlayerStatistics)
+        self.assertEquals(type(self.first_player_stat.player), Player)
