b8c6af26f0eaa0a0d7db9b65c3d0b6a49ccd63c6
Michael Wittig <witmic1@gmail.com>
2016-06-19 13:47:33 +0200

    do not decrypt ofm password in admin

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from users.models import OFMUser
 
-admin.site.register(OFMUser)
+
+@admin.register(OFMUser)
+class VehicleClassAdmin(admin.ModelAdmin):
+    list_display = ['username']
+    search_fields = ['username']
