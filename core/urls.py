d3536159630e475d07f051421c0242763d067ba4
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-06-20 20:56:22 +0200

    added favicon

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 app_name = 'core'
 urlpatterns = [
-    # url(r'^favicon\.ico$', RedirectView.as_view(url='/static/autostew_web_home/img/Logo_RGB.png', permanent=True)),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/core/img/OFM_favicon.png', permanent=True)),
     url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
 ]
