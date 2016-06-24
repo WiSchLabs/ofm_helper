d0482011b9bf7e1f034a142fc7a64115ccc6b426
Michael Wittig <witmic1@gmail.com>
2016-06-24 11:59:31 +0200

    use user installed pip instead

--- devops/update_ofm_helper.sh
+++ devops/update_ofm_helper.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset -o xtrace
 cd repos/ofm_helper
 git pull
 
-pip3 install -r requirements.txt --user
+/home/sh4ke/bin/pip3 install -r requirements.txt --user
 # pip3 install -r prod_requirements.txt --user
 
 export DJANGO_SETTINGS_MODULE=ofm_helper.settings.prod
