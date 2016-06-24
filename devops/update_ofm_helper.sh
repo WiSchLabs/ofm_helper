631d55c421657db4484d805a784ecefe8378a35c
Michael Wittig <witmic1@gmail.com>
2016-06-24 09:51:13 +0200

    deployment script

---
+++ devops/update_ofm_helper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd repos/ofm_helper
+git pull
+
+pip3 install -r requirements.txt --user
+# pip3 install -r prod_requirements.txt --user
+
+export DJANGO_SETTINGS_MODULE=ofm_helper.settings.prod
+
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
