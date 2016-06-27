067ea2127248ebbc6490316f1c1bd1bad1b8d2d5
Michael Wittig <witmic1@gmail.com>
2016-06-27 10:20:22 +0200

    reformatting

--- core/tests/unit/test_views.py
+++ core/tests/unit/test_views.py
@@ -9,7 +9,6 @@ class LoginTestCase(TestCase):
         OFMUser.objects.create_user('temporary', 'temporary@ofmhelper.com', 'temporary')
 
     def test_login_redirects_for_unknown_user(self):
-        
         response = self.client.post(reverse('core:login'), {'username': 'john', 'password': 'smith'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:login'))
@@ -62,7 +61,6 @@ class LoginTestCase(TestCase):
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_view_account_when_logged_in(self):
-
         self.client.login(username='temporary', password='temporary')
         response = self.client.get(reverse('core:account'))
         self.assertEqual(response.status_code, 200)
@@ -82,58 +80,65 @@ class RegistrationTestCase(TestCase):
                                     ofm_password='ofm')
 
     def test_register_new_account(self):
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '1234',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '1234',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:login'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_with_existing_username(self):
-        response = self.client.post(reverse('core:register'), {'username': 'temporary', 'password': '1234', 'password2': '1234',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'temporary', 'password': '1234', 'password2': '1234',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:register'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_with_unmatching_passwords(self):
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '12345',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '12345',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:register'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_with_unmatching_ofm_passwords(self):
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '1234',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'ghj'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '1234',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'abc', 'ofm_password': 'def', 'ofm_password2': 'ghj'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:register'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_with_already_registered_ofm_username(self):
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '1234',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'ofm', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '1234',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'ofm', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:register'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_with_already_registered_email(self):
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '1234',
-                                         'email': 'temporary@ofmhelper.com',
-                                         'ofm_username': 'new', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '1234',
+                                     'email': 'temporary@ofmhelper.com',
+                                     'ofm_username': 'new', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('core:register'))
         self.assertFalse(response.wsgi_request.user.is_authenticated())
 
     def test_register_when_already_logged_in(self):
         self.client.login(username='temporary', password='temporary')
-        response = self.client.post(reverse('core:register'), {'username': 'new', 'password': '1234', 'password2': '1234',
-                                         'email': 'new@ofmhelper.com',
-                                         'ofm_username': 'new', 'ofm_password': 'def', 'ofm_password2': 'def'})
+        response = self.client.post(reverse('core:register'),
+                                    {'username': 'new', 'password': '1234', 'password2': '1234',
+                                     'email': 'new@ofmhelper.com',
+                                     'ofm_username': 'new', 'ofm_password': 'def', 'ofm_password2': 'def'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/account/home.html')
         self.assertTrue(response.wsgi_request.user.is_authenticated())
