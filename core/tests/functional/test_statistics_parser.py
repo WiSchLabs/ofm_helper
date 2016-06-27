d8610f8df20aa6dbaa430e46509fe55db158a6bc
Michael Wittig <witmic1@gmail.com>
2016-06-27 10:17:05 +0200

    use named urls instead of hardcoded links + move parsing view

--- core/tests/functional/test_statistics_parser.py
+++ core/tests/functional/test_statistics_parser.py
@@ -1,25 +1,34 @@
 from unittest import skip
 
-from django.test import TestCase
+from core.configuration_provider import ConfigurationProvider
+from django.test import TestCase, Client
 
 from core.factories.core_factories import MatchdayFactory
 from core.models import Player, PlayerStatistics
 from core.parsers.player_statistics_parser import PlayerStatisticsParser
 from core.web.ofm_page_constants import Constants
 from core.web.site_manager import SiteManager
+from users.models import OFMUser
 
 
 class StatisticsParserTest(TestCase):
     def setUp(self):
-        self.site_manager = SiteManager()
-        self.site_manager.login()
-        self.site_manager.browser.get(Constants.TEAM.PLAYER_STATISTICS)
-        self.assertIn('Spielerstatistik', self.site_manager.browser.title)
-        parser = PlayerStatisticsParser()
-        parser.url = self.site_manager.browser.page_source
-        MatchdayFactory.create()
-        self.player_stat_list = parser.parse()
-        self.first_player_stat = self.player_stat_list[0]
+        config = ConfigurationProvider()
+        ofm_username = config.get('credentials', 'OFM_USERNAME')
+        ofm_password = config.get('credentials', 'OFM_PASSWORD')
+        OFMUser('name', 'mail@pro.com', 'pass', ofm_username=ofm_username, ofm_password=ofm_password).create()
+
+        self.client.login(username='name', password='pass')
+
+        response = self.client.get('/account')
+        self.assertEqual(response.status_code, 200)
+
+        #self.assertIn('Spielerstatistik', self.site_manager.browser.title)
+        #parser = PlayerStatisticsParser()
+        #parser.url = self.site_manager.browser.page_source
+        #MatchdayFactory.create()
+        #self.player_stat_list = parser.parse()
+        #self.first_player_stat = self.player_stat_list[0]
 
     @skip('because i say so, mindestens, digga')
     def test_parsed_player_stat_contains_all_foreign_keys(self):
