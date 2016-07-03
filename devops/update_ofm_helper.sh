c3caa8ba9d80d2f96489cc8098f02bd970e48566
Michael Wittig <witmic1@gmail.com>
2016-07-03 20:45:41 +0200

    skip prod requirements for now

--- devops/update_ofm_helper.sh
+++ devops/update_ofm_helper.sh
@@ -6,7 +6,7 @@ cd repos/ofm_helper
 git pull
 
 /home/sh4ke/bin/pip3 install -r requirements.txt --user
-/home/sh4ke/bin/pip3 install -r prod_requirements.txt --user
+#/home/sh4ke/bin/pip3 install -r prod_requirements.txt --user
 
 export DJANGO_SETTINGS_MODULE=ofm_helper.settings.prod
 
