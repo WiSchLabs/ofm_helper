c2b4c2154bb6c7d2a0761ad70aeae5e5450207af
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-07-08 19:35:32 +0200

    refactored unit test to use model factories instead of the actual parser

--- core/tests/unit/test_ofm_views.py
+++ core/tests/unit/test_ofm_views.py
@@ -1,25 +1,19 @@
-import os
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from core.factories.core_factories import MatchdayFactory
-from core.parsers.player_statistics_parser import PlayerStatisticsParser
+from core.factories.core_factories import MatchdayFactory, PlayerFactory
+from core.models import PlayerUserOwnership
 from users.models import OFMUser
-
-TESTDATA_PATH = 'core/tests/assets'
 
 
 class OFMViewTestCase(TestCase):
     def setUp(self):
-        # todo: refactor to use PlayerStatisticsFactory instead of PlayerStatisticsParser
-        testdata = open(os.path.join(TESTDATA_PATH, 'player_statistics.html'), encoding='utf8')
-        MatchdayFactory.create()
+        player = PlayerFactory.create()
+        matchday = MatchdayFactory.create()
         self.user1 = OFMUser.objects.create_user('alice', 'alice@ofmhelper.com', 'alice', ofm_username='alice', ofm_password='alice')
         self.user2 = OFMUser.objects.create_user('bob', 'bob@ofmhelper.com', 'bob', ofm_username='bob', ofm_password='bob')
+        PlayerUserOwnership.objects.create(user=self.user1, player=player, bought_on_matchday=matchday, sold_on_matchday=None)
         self.client.login(username='alice', password='alice')
-        self.parser = PlayerStatisticsParser(testdata, self.user1)
-        self.player_stat_list = self.parser.parse()
 
     def test_user_can_see_his_player_list(self):
         response = self.client.get(reverse('core:ofm:player_list'))
