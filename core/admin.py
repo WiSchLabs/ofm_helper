95364f0fc3a84116ba4c073a947b4f36ec1098b1
Michael Wittig <witmic1@gmail.com>
2016-07-09 09:38:06 +0200

    refining admin page

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,48 @@
 from core.models import Season, Quarter, Country, Matchday, League, Player, PlayerStatistics
 from django.contrib import admin
 
-admin.site.register(Season)
-admin.site.register(Quarter)
-admin.site.register(Country)
-admin.site.register(Matchday)
-admin.site.register(League)
-admin.site.register(Player)
-admin.site.register(PlayerStatistics)
 
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_filter = ['number']
+    list_display = ['number', ]
+    search_fields = ['number', ]
+
+
+@admin.register(Quarter)
+class QuarterAdmin(admin.ModelAdmin):
+    list_filter = ['quarter']
+    list_display = ['season', 'quarter', ]
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_filter = ['country']
+    list_display = ['country', ]
+    search_fields = ['country', ]
+
+
+@admin.register(Matchday)
+class MatchdayAdmin(admin.ModelAdmin):
+    list_display = ['season', 'number', ]
+    search_fields = ['number', ]
+
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ['league', 'relay', 'country']
+    search_fields = ['league', ]
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_filter = ['position', 'nationality']
+    list_display = ['name', ]
+    search_fields = ['name', ]
+
+
+@admin.register(PlayerStatistics)
+class SeasonAdmin(admin.ModelAdmin):
+    list_filter = ['player', 'matchday']
+    list_display = ['player', 'matchday']
+    search_fields = ['player', ]
