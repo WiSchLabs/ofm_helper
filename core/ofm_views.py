eca1797d740b7607df12f602209ec73a53ef2c4d
Michael Wittig <witmic1@gmail.com>
2016-07-12 11:36:21 +0200

    data polishing for table

--- core/ofm_views.py
+++ core/ofm_views.py
@@ -1,7 +1,6 @@
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from core.models import Player, Contract, PlayerStatistics
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, TemplateView, View
 
@@ -52,21 +51,21 @@ class PlayerDataAsJsonView(CsrfExemptMixin, JsonRequestResponseMixin, View):
 
         statistic_diff = dict()
         statistic_diff['position'] = st1.player.position
+        statistic_diff['strength'] = st1.strength
         statistic_diff['name'] = st1.player.name
-        statistic_diff['ep'] = st1.ep - st2.ep if st2 else st1.ep
-        statistic_diff['tp'] = st1.tp - st2.tp if st2 else st1.tp
-        statistic_diff['awp'] = st1.awp - st2.awp if st2 else st1.awp
-        statistic_diff['strength'] = st1.strength - st2.strength if st2 else st1.strength
-        statistic_diff['freshness'] = st1.freshness - st2.freshness if st2 else st1.freshness
-        statistic_diff['games_in_season'] = st1.games_in_season - st2.games_in_season if st2 else st1.games_in_season
-        statistic_diff['goals_in_season'] = st1.goals_in_season - st2.goals_in_season if st2 else st1.goals_in_season
-        statistic_diff['won_tacklings_in_season'] = st1.won_tacklings_in_season - st2.won_tacklings_in_season if st2 else st1.won_tacklings_in_season
-        statistic_diff['lost_tacklings_in_season'] = st1.lost_tacklings_in_season - st2.lost_tacklings_in_season if st2 else st1.lost_tacklings_in_season
-        statistic_diff['won_friendly_tacklings_in_season'] = st1.won_friendly_tacklings_in_season - st2.won_friendly_tacklings_in_season if st2 else st1.won_friendly_tacklings_in_season
-        statistic_diff['lost_friendly_tacklings_in_season'] = st1.lost_friendly_tacklings_in_season - st2.lost_friendly_tacklings_in_season if st2 else st1.lost_friendly_tacklings_in_season
-        statistic_diff['yellow_cards_in_season'] = st1.yellow_cards_in_season - st2.yellow_cards_in_season if st2 else st1.yellow_cards_in_season
-        statistic_diff['red_cards_in_season'] = st1.red_cards_in_season - st2.red_cards_in_season if st2 else st1.red_cards_in_season
-        statistic_diff['equity'] = st1.equity - st2.equity if st2 else st1.equity
+        statistic_diff['ep'] = "%s (%s)" % (st1.ep, st1.ep - st2.ep if st2 else '')
+        statistic_diff['tp'] = "%s (%s)" % (st1.tp, st1.tp - st2.tp if st2 else '')
+        statistic_diff['awp'] = "%s (%s)" % (st1.awp, st1.awp - st2.awp if st2 else '')
+        statistic_diff['freshness'] = "%s (%s)" % (st1.freshness, st1.freshness - st2.freshness if st2 else '')
+        statistic_diff['games_in_season'] = st1.games_in_season
+        statistic_diff['goals_in_season'] = st1.goals_in_season
+        statistic_diff['won_tacklings_in_season'] = st1.won_tacklings_in_season
+        statistic_diff['lost_tacklings_in_season'] = st1.lost_tacklings_in_season
+        statistic_diff['won_friendly_tacklings_in_season'] = st1.won_friendly_tacklings_in_season
+        statistic_diff['lost_friendly_tacklings_in_season'] = st1.lost_friendly_tacklings_in_season
+        statistic_diff['yellow_cards_in_season'] = st1.yellow_cards_in_season
+        statistic_diff['red_cards_in_season'] = st1.red_cards_in_season
+        statistic_diff['equity'] = st1.equity
 
         return statistic_diff
 
