dd5f3ac300eded3f7a0c7b26fd402112dbeec1d4
Michael Wittig <witmic1@gmail.com>
2016-07-12 15:24:41 +0200

    delete obsolete tests

--- core/tests/unit/test_ofm_views.py
+++ core/tests/unit/test_ofm_views.py
@@ -16,17 +16,6 @@ class OFMViewTestCase(TestCase):
         Contract.objects.create(user=self.user1, player=self.player, bought_on_matchday=self.matchday, sold_on_matchday=None)
         self.client.login(username='alice', password='alice')
 
-    def test_user_can_see_his_player_list(self):
-        response = self.client.get(reverse('core:ofm:player_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(len(response.context_data['player_list']) > 0)
-
-    def test_user_cannot_see_other_users_player_list(self):
-        self.client.login(username='bob', password='bob')
-        response = self.client.get(reverse('core:ofm:player_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(len(response.context_data['player_list']) > 0)
-
     def test_user_can_see_his_players(self):
         response = self.client.get('/ofm/players/'+str(self.player.id))
         self.assertEqual(response.status_code, 200)
@@ -52,7 +41,7 @@ class OFMViewTestCase(TestCase):
         returned_json_data = json.loads(response.content.decode('utf-8'))
         self.assertEquals(len(returned_json_data), 1)
         self.assertEquals(returned_json_data[0]['position'], 'TW')
-        self.assertEquals(returned_json_data[0]['name'], 'Martin Adomeit')
+        self.assertEquals(returned_json_data[0]['name'], '<a href="/ofm/players/1">Martin Adomeit</a>')
         self.assertEquals(returned_json_data[0]['ep'], '2 ()')
         self.assertEquals(returned_json_data[0]['tp'], '5 ()')
         self.assertEquals(returned_json_data[0]['awp'], '3 ()')
@@ -69,7 +58,7 @@ class OFMViewTestCase(TestCase):
         returned_json_data = json.loads(response.content.decode('utf-8'))
         self.assertEquals(len(returned_json_data), 1)
         self.assertEquals(returned_json_data[0]['position'], 'TW')
-        self.assertEquals(returned_json_data[0]['name'], 'Martin Adomeit')
+        self.assertEquals(returned_json_data[0]['name'], '<a href="/ofm/players/1">Martin Adomeit</a>')
         self.assertEquals(returned_json_data[0]['ep'], '3 (1)')
         self.assertEquals(returned_json_data[0]['tp'], '6 (1)')
         self.assertEquals(returned_json_data[0]['awp'], '4 (1)')
@@ -87,7 +76,7 @@ class OFMViewTestCase(TestCase):
         returned_json_data = json.loads(response.content.decode('utf-8'))
         self.assertEquals(len(returned_json_data), 1)
         self.assertEquals(returned_json_data[0]['position'], 'TW')
-        self.assertEquals(returned_json_data[0]['name'], 'Martin Adomeit')
+        self.assertEquals(returned_json_data[0]['name'], '<a href="/ofm/players/1">Martin Adomeit</a>')
         self.assertEquals(returned_json_data[0]['ep'], '2 ()')
         self.assertEquals(returned_json_data[0]['tp'], '5 ()')
         self.assertEquals(returned_json_data[0]['awp'], '3 ()')
