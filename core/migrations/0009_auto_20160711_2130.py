4078b103088a0346893d23f3d173b861d3685143
Michael Wittig <witmic1@gmail.com>
2016-07-16 19:09:00 +0200

    fix migrate merge erros

--- core/migrations/0009_auto_20160711_2130.py
+++ core/migrations/0009_auto_20160711_2130.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0007_auto_20160711_1001'),
+        ('core', '0008_auto_20160711_1128'),
     ]
 
     operations = [
