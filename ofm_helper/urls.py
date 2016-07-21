9fa701589a64b3e9c8fda7ceac636fdb8ed50f65
Michael Wittig <witmic1@gmail.com>
2016-07-21 08:08:40 +0200

    Revert "remove docker functionality"
    This reverts commit 31b0d8070b03255e705baf3ba5cd7e73e1e8489c.

--- ofm_helper/urls.py
+++ ofm_helper/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls'), name='core'),
 ]
+
+if settings.DEBUG is False:  # if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+                            url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                                {'document_root': settings.STATIC_ROOT}),
+                            )
