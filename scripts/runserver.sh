ac74962084d64e1cbbd329551adfae977959a5f9
sebastian.schreck <sebastian.schreck@mailbox.tu-berlin.de>
2016-07-28 23:52:02 +0200

    backslashes are evil

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -3,7 +3,6 @@
 export DJANGO_SETTINGS_MODULE=ofm_helper.settings.prod
 
 python3 manage.py migrate
-echo "from users.models import OFMUser; if not OFMUser.objects.get(username='admin'): \
-        OFMUser.objects.create_user(username='admin', password='admin', is_staff=True, is_superuser=True)" \
+echo "from users.models import OFMUser; if not OFMUser.objects.get(username='admin'): OFMUser.objects.create_user(username='admin', password='admin', is_staff=True, is_superuser=True)" \
     | python manage.py shell
 python3 manage.py runserver 0.0.0.0:8000
