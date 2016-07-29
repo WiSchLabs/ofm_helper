f85c73a659f0b8638f4f98582573651399f7cd4b
Michael Wittig <witmic1@gmail.com>
2016-07-29 10:31:10 +0200

    we don need no ... admin?

--- scripts/create_admin.py
+++
@@ -1,4 +0,0 @@
-from users.models import OFMUser
-
-if not OFMUser.objects.get(username='admin'):
-    OFMUser.objects.create_user(username='admin', password='admin', is_staff=True, is_superuser=True)
--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -3,6 +3,4 @@
 export DJANGO_SETTINGS_MODULE=ofm_helper.settings.prod
 
 python3 manage.py migrate
-
-cat scripts/create_admin.py | python manage.py shell
 python3 manage.py runserver 0.0.0.0:8000
