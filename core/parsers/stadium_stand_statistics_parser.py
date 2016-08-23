45d1c64b9de185ff3f590aabe4215c4ad205d851
sebastian.schreck <sebastian.schreck@stegschreck.de>
2016-08-23 18:55:48 +0200

    don't parse stands, if they were not used in the last match

--- core/parsers/stadium_stand_statistics_parser.py
+++ core/parsers/stadium_stand_statistics_parser.py
@@ -34,22 +34,27 @@ class StadiumStandStatisticsParser(BaseParser):
 
     def _parse_stand_statistics(self, stand_data):
         if 'nonvisible' in stand_data['class']:
+            # stand is currently under construction or in repair
             return
+
         has_roof = 'überdacht' in stand_data.find('h1').get_text()
         has_seats = 'Sitzplätze' in stand_data.find('h1').get_text()
         capacity = stand_data.find_all('tr')[2].find_all('td')[3].div.get_text().replace('.', '')
-
-        stand_level, success = StandLevel.objects.get_or_create(
-            capacity=capacity,
-            has_roof=has_roof,
-            has_seats=has_seats
-        )
 
         sector = stand_data.find('span', class_='white').get_text()[0]
         condition = stand_data.find_all('tr')[2].find_all('td')[0].find_all('span')[1].get_text().replace(',', '.').replace('%', '')
         visitors = stand_data.find_all('tr')[3].find_all('td')[2].span.get_text().replace('.', '')
         ticket_price = stand_data.find_all('tr')[6].find_all('select')[0].find('option', selected=True).get('value')
 
+        if visitors == '-':
+            # stadium was not used in the last match (e.g. due to repair in progress)
+            return
+
+        stand_level, success = StandLevel.objects.get_or_create(
+            capacity=capacity,
+            has_roof=has_roof,
+            has_seats=has_seats
+        )
         stadium_stand_stat, success = StadiumStandStatistics.objects.get_or_create(
             stadium_statistics=self.match_stadium_stat,
             sector=sector,
