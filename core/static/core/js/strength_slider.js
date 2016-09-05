03231d0408c82af79a4a062a5a83c9c5e58be876
Michael Wittig <witmic1@gmail.com>
2016-09-05 16:39:28 +0200

    move strength slider js to seperate file

---
+++ core/static/core/js/strength_slider.js
@@ -0,0 +1,41 @@
+var $tables = $('.statistics_table')
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$("#strength-slider-label").html('Gemittelte Stärke');
+$("#strength-slider-label").attr('title', "Harmonisches Mittel der Mannschaftsstärken");
+$("#strength-slider-label").attr('data-original-title', "Harmonisches Mittel der Mannschaftsstärken");
+
+$("#strength-slider").slider({labelledby: 'strength-slider-label'});
+
+function get_current_params(){
+    var strength1 = parseInt($('#strength-slider').attr('value').split(',')[0], 10);
+    var strength2 = parseInt($('#strength-slider').attr('value').split(',')[1], 10);
+
+    var params = {
+        tolerance: $('#tolerance-slider').attr('value'),
+        harmonic_strength: Number(2*strength1*strength2/(strength1+strength2)).toFixed(0)
+    };
+
+    return params;
+}
+
+function update_stadium_statistics() {
+    var params = get_current_params();
+
+    $.get(JSON_URL, params, function (returnedData) {
+            $tables.bootstrapTable('removeAll');
+            $tables.bootstrapTable('load', returnedData);
+        }
+    );
+}
+
+$('#strength-slider').slider().on('slideStop', function(event){
+    update_stadium_statistics()
+});
+
+$('#tolerance-slider').slider().on('slideStop', function(event){
+    update_stadium_statistics()
+});
