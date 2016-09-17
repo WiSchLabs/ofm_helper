9511d1a9275174253b2c3f4cd9ecb98e63c2e1b7
Michael Wittig <witmic1@gmail.com>
2016-09-17 20:42:01 +0200

    only import patterns if we are in production mode

--- ofm_helper/urls.py
+++ ofm_helper/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG is False:  # if DEBUG is True it will be served automatically
+    from django.conf.urls import patterns
     urlpatterns += patterns('',
                             url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
                                 {'document_root': settings.STATIC_ROOT}),
