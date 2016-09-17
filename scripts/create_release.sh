6f7649a6791300e773147e10c3565e6cec5cf432
Michael Wittig <witmic1@gmail.com>
2016-09-17 19:29:35 +0200

    travis does not like ofmhelper.de lulwhut?

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -2,7 +2,7 @@
 
 mkdir release
 cd release
-wget https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
+wget --no-check-certificate https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
 tar -xzf ofmhelper2exe.tar.gz
 rm ofmhelper2exe.tar.gz
 ls .. | grep -v release | xargs -i cp -r ../{} ofm_helper/.
