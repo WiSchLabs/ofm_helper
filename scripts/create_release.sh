ba504dd7e93ae558d5c0bf6c1d1830446ef9af28
Michael Wittig <witmic1@gmail.com>
2016-09-17 20:09:48 +0200

    ommit seperate release folder in zip

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -6,5 +6,5 @@ wget --no-check-certificate https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
 tar -xzf ofmhelper2exe.tar.gz
 rm ofmhelper2exe.tar.gz
 ls .. | grep -v release | xargs -i cp -r ../{} ofm_helper/.
-cd ..
-zip -qdgds 10m -r ofm_helper.zip release/*
+zip -qdgds 10m -r ofm_helper.zip *
+mv ofm_helper.zip ..
