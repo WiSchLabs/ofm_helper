0a7191d85221716590ee28025fd1aa1f9b90d192
Michael Wittig <witmic1@gmail.com>
2016-09-17 19:23:44 +0200

    automatic commits via travis

---
+++ scripts/create_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir release
+cd release
+wget https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
+tar -xzf ofmhelper2exe.tar.gz
+rm ofmhelper2exe.tar.gz
+ls .. | grep -v release | xargs -i cp -r ../{} ofm_helper/.
+cd ..
+zip -r ofm_helper.zip release/*
