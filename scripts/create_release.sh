12e07a350fd03e00dcec80a1bb11817d33a72232
sebastian.schreck <sebastian.schreck@stegschreck.de>
2016-10-30 08:16:50 +0100

    move version file creation for exe

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+mkdir release
+cd release
+
 touch .version
 echo `git describe --tags --always` | awk '{split($0,a,"-"); print a[1]}' > .version
 
-mkdir release
-cd release
 wget --no-check-certificate https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
 tar -xzf ofmhelper2exe.tar.gz
 rm ofmhelper2exe.tar.gz
