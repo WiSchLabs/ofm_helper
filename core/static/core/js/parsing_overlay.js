d9374bc3f0965a6912c7418412f50ca33d890f29
sebastian.schreck <sebastian.schreck@stegschreck.de>
2016-10-30 11:42:38 +0100

    changed parsing button icon

--- core/static/core/js/parsing_overlay.js
+++ core/static/core/js/parsing_overlay.js
@@ -1,4 +1,4 @@
-$("form.parseTrigger").append("<button type='submit' class='btn btn-default' id='OfmParseTrigger'><span class='glyphicon glyphicon-refresh'></span> jetzt diese Kategorie parsen</button>");
+$("form.parseTrigger").append("<button type='submit' class='btn btn-default' id='OfmParseTrigger'><span class='glyphicon glyphicon-download'></span> jetzt diese Kategorie parsen</button>");
 $('#OfmParseTrigger').click(function() {
     $("body").append("<div class='overlay'><div id='loading-img'><span>Die Daten werden geladen. Bitte Geduld.</span></div></div>");
     $(".overlay").show();
