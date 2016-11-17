a96e12982b7cabe5456f33bb69d6a15542c1a461
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2016-11-17 14:42:53 +0100

    added unit test

--- core/tests/unit/views/test_ofm_matches_view.py
+++ core/tests/unit/views/test_ofm_matches_view.py
@@ -1,9 +1,9 @@
 import json
 
-from core.factories.core_factories import MatchdayFactory, PlayerFactory, PlayerStatisticsFactory, MatchFactory
-from core.models import Contract
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+from core.factories.core_factories import MatchdayFactory, MatchFactory
 from users.models import OFMUser
 
 
@@ -33,4 +33,10 @@ class OFMMatchesViewTestCase(TestCase):
         self.assertTrue('0:0' in returned_json_data[0]['result'])
         self.assertTrue('Olympiastadion Berlin' in returned_json_data[0]['venue'])
 
-
+    def test_user_can_see_matches_summary(self):
+        response = self.client.get(reverse('core:ofm:matches_summary_json'))
+        self.assertEqual(response.status_code, 200)
+        returned_json_data = json.loads(response.content.decode('utf-8'))
+        self.assertEquals(0, returned_json_data['matches_won'])
+        self.assertEquals(1, returned_json_data['matches_draw'])
+        self.assertEquals(0, returned_json_data['matches_lost'])
