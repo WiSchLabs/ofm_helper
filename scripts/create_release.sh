72bca75f4253a0df6c49bdfc9f8c69c76630a74c
Michael Wittig <witmic1@gmail.com>
2016-11-26 20:12:42 +0100

    fix create release script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -9,6 +9,6 @@ cd release
 wget --no-check-certificate https://ofmhelper.de/downloads/ofmhelper2exe.tar.gz
 tar -xzf ofmhelper2exe.tar.gz
 rm ofmhelper2exe.tar.gz
-ls .. | grep -v release | xargs -i cp -r ../{} ofm_helper/.
+ls .. | grep -v release | xargs -i cp -r ../{} ofmhelper2exe/ofm_helper/
 zip -qdgds 10m -r ofm_helper.zip *
 mv ofm_helper.zip ..
