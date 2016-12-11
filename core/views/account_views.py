bd66dff8cb61aa245460a47de10418fb26680388
Michael Wittig <witmic1@gmail.com>
2016-12-11 10:33:22 +0100

    #99 extract more strings into localization module

--- core/views/account_views.py
+++ core/views/account_views.py
@@ -2,13 +2,16 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
-from core.localization.messages import MSG_PASSWORDS_UNEQUAL, MSG_OFM_PASSWORDS_UNEQUAL, MSG_NOT_LOGGED_IN
+from core.localization.messages import MSG_PASSWORDS_UNEQUAL, MSG_OFM_PASSWORDS_UNEQUAL, MSG_NOT_LOGGED_IN, \
+    OFM_USERNAME_ALREADY_EXISTS, USERNAME_ALREADY_EXISTS, EMAIL_ALREADY_EXISTS, MSG_ALREADY_LOGGED_IN, \
+    MSG_ACCOUNT_CREATED, LOGGED_OUT, USERNAME_OR_PASSWORD_INVALID, LOGIN_IMPOSSIBLE_ACCOUNT_IS_DEACTIVATED, \
+    LOGIN_SUCCESSFUL
 from users.models import OFMUser
 
 
 def register_view(request):
     if request.user.is_authenticated():
-        messages.error(request, "Du bist bereits eingeloggt. Du kannst dich im Menü ausloggen.")
+        messages.error(request, MSG_ALREADY_LOGGED_IN)
         return render(request, 'core/account/home.html')
     if request.POST:
         username = request.POST.get('username')
@@ -20,11 +23,11 @@ def register_view(request):
         ofm_password2 = request.POST.get('ofm_password2')
 
         if OFMUser.objects.filter(email=email).exists():
-            messages.error(request, "Ein Account mit dieser E-Mail-Adresse existiert bereits.")
+            messages.error(request, EMAIL_ALREADY_EXISTS)
             return redirect('core:register')
 
         if OFMUser.objects.filter(username=username).exists():
-            messages.error(request, "Ein Account mit diesem Benutzernamen existiert bereits.")
+            messages.error(request, USERNAME_ALREADY_EXISTS)
             return redirect('core:register')
 
         if password != password2:
@@ -32,7 +35,7 @@ def register_view(request):
             return redirect('core:register')
 
         if OFMUser.objects.filter(ofm_username=ofm_username).exists():
-            messages.error(request, "Es existiert bereits ein Account für diesen OFM Benutzernamen.")
+            messages.error(request, OFM_USERNAME_ALREADY_EXISTS)
             return redirect('core:register')
 
         if ofm_password != ofm_password2:
@@ -47,7 +50,7 @@ def register_view(request):
             ofm_password=ofm_password,
         )
 
-        messages.success(request, "Account wurde erstellt. Jetzt kannst du dich einloggen.")
+        messages.success(request, MSG_ACCOUNT_CREATED)
         return redirect('core:login')
 
     else:
@@ -62,13 +65,13 @@ def login_view(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                messages.success(request, "Login erfolgreich.")
+                messages.success(request, LOGIN_SUCCESSFUL)
                 return render(request, 'core/account/home.html')
             else:
-                messages.error(request, "Login nicht möglich. Dein Account wurde deaktiviert.")
+                messages.error(request, LOGIN_IMPOSSIBLE_ACCOUNT_IS_DEACTIVATED)
                 return redirect('core:login')
         else:
-            messages.error(request, "Benutzername und/oder Passwort nicht korrekt.")
+            messages.error(request, USERNAME_OR_PASSWORD_INVALID)
             return redirect('core:login')
     else:
         if request.user.is_authenticated():
@@ -80,7 +83,7 @@ def login_view(request):
 def logout_view(request):
     if request.user.is_authenticated():
         logout(request)
-        messages.success(request, "Du wurdest abgemeldet.")
+        messages.success(request, LOGGED_OUT)
     return redirect('core:home')
 
 
