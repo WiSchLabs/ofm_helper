b65551fcae624ae35a093ce7909a2fef3da63e51
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2016-12-31 12:03:50 +0100

    move user registration save to form

--- core/forms/registration_form.py
+++ core/forms/registration_form.py
@@ -7,3 +7,15 @@ class RegistrationForm(ModelForm):
     class Meta:
         model = OFMUser
         fields = ['username', 'password', 'email', 'ofm_username', 'ofm_password']
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+
+        user.username = self.cleaned_data['username']
+        user.set_password(self.cleaned_data['password'])
+        user.email = self.cleaned_data['email']
+        user.ofm_username = self.cleaned_data['ofm_username']
+        user.ofm_password = self.cleaned_data['ofm_password']
+
+        if commit:
+            user.save()
