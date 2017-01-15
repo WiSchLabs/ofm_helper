0db1dd909dcd32aaab0d96265a676de0b6612f5c
Michael Wittig <witmic1@gmail.com>
2017-01-15 10:28:57 +0100

    configuring gunicorn worker timeout to threee minutes

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,6 +3,7 @@ from multiprocessing import cpu_count
 bind = '0.0.0.0:8000'
 reload = True
 
-workers = 2 * cpu_count() + 1
 worker_class = 'eventlet'
+workers = 2 * cpu_count() + 1
 threads = 2 * cpu_count()
+timeout = 180
