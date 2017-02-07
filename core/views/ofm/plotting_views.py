7857d6761e2aef95a59ea81725929e63553c6543
Michael Wittig <witmic1@gmail.com>
2017-02-07 11:00:30 +0100

    #19 a bit of polishing

--- core/views/ofm/plotting_views.py
+++ core/views/ofm/plotting_views.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot
 from matplotlib import style
 from matplotlib import ticker
 from matplotlib.backends.backend_agg import FigureCanvas
+from matplotlib.ticker import MultipleLocator
 
 from core.managers.panda_manager import PandaManager
 
@@ -18,23 +19,39 @@ style.use('ggplot')
 def render_plot(request):
     panda_manager = PandaManager()
 
-    prices = panda_manager.get_grouped_prices('Strength', positions=['MS'], ages=[17])
+    prices = panda_manager.get_grouped_prices('Strength', positions=['MS'], ages=[32], max_price=3*10**7)
 
     fig = pyplot.figure(figsize=(16, 9), dpi=120)
-    ax = fig.add_subplot(111)
+    ax = fig.add_subplot(1, 1, 1)
 
     x = numpy.array(prices.mean().index)
     y = prices.mean()
-    yerr = prices.std()
+    y_error = prices.std()
 
+    ax.xaxis.set_major_locator(MultipleLocator(1))
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+    ax.yaxis.set_minor_locator(MultipleLocator(_get_biggest_power_which_is_smaller_than_max_data(prices)))
+    ax.tick_params(which='both', direction='out', length=4, width=1)
 
-    pyplot.errorbar(x, y, yerr=yerr, fmt='o')
+    ax.grid(which='minor', alpha=0.4)
+    ax.grid(which='major', alpha=0.7)
+
+    pyplot.errorbar(x, y, yerr=y_error, fmt='o', color='g')
+    pyplot.ylabel('Preis')
+    pyplot.title('Spielerpreise')
 
     canvas = FigureCanvas(fig)
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
     return response
+
+
+def _get_biggest_power_which_is_smaller_than_max_data(prices):
+    max_price = prices.mean().max()
+    i = 0
+    while 10 ** i < max_price:
+        i += 1
+    return 10 ** (i-2)
 
 
 @method_decorator(login_required, name='dispatch')
