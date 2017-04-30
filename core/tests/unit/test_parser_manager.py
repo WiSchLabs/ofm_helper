86e93fa6fcf903d2fec49d27dc2053819a962773
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-04-30 08:17:21 +0200

    #19 SiteManager was renamed to OFMSiteManager

--- core/tests/unit/test_parser_manager.py
+++ core/tests/unit/test_parser_manager.py
@@ -10,7 +10,7 @@ class ParserManagerTest(TestCase):
 
     @patch('core.parsers.ofm_helper_version_parser.OfmHelperVersionParser.parse')
     def test_parse_ofm_version(self, parse_version_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_ofm_version(site_manager_mock)
 
@@ -18,7 +18,7 @@ class ParserManagerTest(TestCase):
 
     @patch('core.parsers.matchday_parser.MatchdayParser.parse')
     def test_parse_matchday(self, parse_matchday_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_matchday(site_manager_mock)
 
@@ -27,7 +27,7 @@ class ParserManagerTest(TestCase):
     @patch('core.managers.parser_manager.ParserManager.parse_matchday')
     @patch('core.parsers.players_parser.PlayersParser.parse')
     def test_parse_players_and_matchday(self, parse_players_mock, parse_matchday_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_players(site_manager_mock)
 
@@ -38,7 +38,7 @@ class ParserManagerTest(TestCase):
     @patch('core.parsers.player_statistics_parser.PlayerStatisticsParser.parse')
     def test_parse_player_statistics(self, parse_player_statistics_mock, parse_matchday_mock):
         self.parser_manager.players_already_parsed = True
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_player_statistics(site_manager_mock)
 
@@ -48,7 +48,7 @@ class ParserManagerTest(TestCase):
     @patch('core.managers.parser_manager.ParserManager.parse_players')
     @patch('core.parsers.player_statistics_parser.PlayerStatisticsParser.parse')
     def test_parse_player_statistics_and_matchday(self, parse_player_statistics_mock, parse_players_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_player_statistics(site_manager_mock)
 
@@ -58,7 +58,7 @@ class ParserManagerTest(TestCase):
     @patch('core.managers.parser_manager.ParserManager.parse_matchday')
     @patch('core.parsers.awp_boundaries_parser.AwpBoundariesParser.parse')
     def test_parse_awp_and_matchday(self, parse_awp_mock, parse_matchday_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_awp_boundaries(site_manager_mock)
 
@@ -68,7 +68,7 @@ class ParserManagerTest(TestCase):
     @patch('core.managers.parser_manager.ParserManager.parse_matchday')
     @patch('core.parsers.finances_parser.FinancesParser.parse')
     def test_parse_finances_and_matchday(self, parse_finances_mock, parse_matchday_mock):
-        with patch('core.managers.site_manager.SiteManager') as site_manager_mock:
+        with patch('core.managers.site_manager.OFMSiteManager') as site_manager_mock:
             site_manager_mock.browser.page_source.return_value = "<html></html>"
             self.parser_manager.parse_finances(site_manager_mock)
 
